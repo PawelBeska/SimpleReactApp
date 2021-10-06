08919efa50e0e88b53533731f06ad038dd3f96b3
Paweł Beśka <raz4r@interia.pl>
2021-10-06 22:53:56 +0200

    Project init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import HeaderComponent from './components/global/HeaderComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HeaderComponent></HeaderComponent>
+    </div>
+  );
+}
+
+export default App;
+

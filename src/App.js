5707ac202a3cd7db239a7113c9cbfaa73ab2cbf2
Paweł Beśka <raz4r@interia.pl>
2021-10-06 23:01:55 +0200

    Adding IndexBodyComponent

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 import HeaderComponent from './components/global/HeaderComponent';
+import IndexBodyComponent from "./components/pages/index/IndexBodyComponent";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from "react-bootstrap/Container";
+import {Col, Row} from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <HeaderComponent></HeaderComponent>
-    </div>
-  );
+    return (
+        <div className="App">
+            <HeaderComponent></HeaderComponent>
+            <Container className={"mt-5"}>
+                <Row>
+                    <Col>
+                        <IndexBodyComponent></IndexBodyComponent>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/pages/index/IndexBodyComponent.js
@@ -0,0 +1,8 @@
+function IndexBodyComponent()
+{
+    return(<h1>
+        Strona główna
+    </h1>);
+}
+
+export default IndexBodyComponent;
